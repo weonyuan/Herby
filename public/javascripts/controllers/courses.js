d56ad6eaa412ac52ed2f42f49ea5a15e67c20207
Weon Yuan <weon.yuan@gmail.com>
2015-11-15 22:11:28 -0500

    Implement ng-repeat to render 3 courses upon init.

--- public/javascripts/controllers/courses.js
+++ public/javascripts/controllers/courses.js
@@ -9,11 +9,17 @@ app.controller('CoursesCtrl',
     numCourses: 3
   }
 
-  $scope.form = { subject: null, courseNum: null}
+  $scope.form = [
+    { subject: null, courseNum: null },
+    { subject: null, courseNum: null },
+    { subject: null, courseNum: null }
+  ]
+
   $scope.updateSelection = function() {
     console.log($scope.form);
     console.log($scope.data.courses[$scope.form.subject]);
   };
 
-  console.log($scope.data.courses['MATH']);
+  console.log($scope.data.numCourses);
+  console.log($scope.form);
 }]);
